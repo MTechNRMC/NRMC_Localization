667c94207510b0a8daa3867461af50da70ccbd8f
GammaGames <jesse007@ymail.com>
2017-04-11 14:39:35 -0600

    Removed unnecessary code and made testing code work with two

--- 2017_Localization/LeddarInterface.py
+++ 2017_Localization/LeddarInterface.py
@@ -2,7 +2,7 @@ import minimalmodbus
 import time
 
 class LeddarInterface:
-  # 8.8 Hz
+  # 8.8Hz, CANNOT CHANGE CURRENTLY WITHOUT CHECKSUM ERRORS
   LEDDAR_FREQUENCY = 1/8.8
   cached_read_time = 0
   cached_distance = -1
@@ -12,16 +12,13 @@ class LeddarInterface:
     self.leddar = minimalmodbus.Instrument(leddar_com_port, leddar_address)
 
   def read_distance(self):
-    try:
-      distance = self.leddar.read_register(24, 0, 4)/1000
-      time = ((self.leddar.read_register(21, 0, 4) << 16) + self.leddar.read_register(20, 0, 4))/1000
-      if time == self.cached_read_time:
-        return False
-      self.cached_distance = distance
-      self.cached_read_time = time
-      return True
-    except OSError:
+    distance = self.leddar.read_register(24, 0, 4)/1000
+    time = ((self.leddar.read_register(21, 0, 4) << 16) + self.leddar.read_register(20, 0, 4))/1000
+    if time == self.cached_read_time:
       return False
+    self.cached_distance = distance
+    self.cached_read_time = time
+    return True
 
   # Returns distance in meters to whatever the leddar is looking at
   def get_distance(self):
--- 2017_Localization/LocalizationTestDriver.py
+++ 2017_Localization/LocalizationTestDriver.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 import sys, math
 from Localization import Localization
+from LeddarInterface import LeddarInterface
 from LeddarPositionInterface import LeddarPositionInterface
 from Position import Position
 
 leddar1_com_port = '/dev/ttyUSB0'
 leddar1_address = 1
-# Only have one leddar for now
-leddar2_com_port = '/dev/ttyUSB0'
+leddar2_com_port = '/dev/ttyUSB1'
 leddar2_address = 1
 
 def main(argv):
   top_post_position = Position(float(argv[0]), float(argv[1]))
   bottom_post_position = Position(float(argv[2]), float(argv[3]))
-  leddar_interface = LeddarPositionInterface(leddar1_com_port, leddar1_address, 
+  positioner = LeddarPositionInterface(leddar1_com_port, leddar1_address,
     leddar2_com_port, leddar2_address, 
     top_post_position, bottom_post_position)
 
-  print("Measured {0}".format(leddar_interface.get_position()))
+  for i in range(0,1000):
+    print("At {0}".format(positioner.get_position())
+
 
 if __name__ == "__main__":
   main(sys.argv[1:])
