1b03cda44d414669a0548cba3500f0172a1b7182
Logan Warner <loganswarneprofessional@gmail.com>
2017-05-09 11:14:21 -0600

    Fix up some minor issues with the test driver

--- 2017_Localization/LocalizationTestDriver.py
+++ 2017_Localization/LocalizationTestDriver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys, math
+import sys
+import math
 from Localization import Localization
 from LeddarInterface import LeddarInterface
 from LeddarPositionInterface import LeddarPositionInterface
@@ -10,15 +11,16 @@ leddar1_address = 1
 leddar2_com_port = '/dev/ttyUSB1'
 leddar2_address = 1
 
+
 def main(argv):
   top_post_position = Position(float(argv[0]), float(argv[1]))
   bottom_post_position = Position(float(argv[2]), float(argv[3]))
   positioner = LeddarPositionInterface(leddar1_com_port, leddar1_address,
-    leddar2_com_port, leddar2_address, 
-    top_post_position, bottom_post_position)
+                                      leddar2_com_port, leddar2_address,
+                                      top_post_position, bottom_post_position)
 
-  for i in range(0,1000):
-    print("At {0}".format(positioner.get_position())
+  for i in range(0, 1000):
+    print("At {0}".format(positioner.get_position()))
 
 
 if __name__ == "__main__":
